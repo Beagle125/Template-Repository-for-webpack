167e87bddb1103bba1676f36e81d9f5b1694a2c9
cedric_daniel_chan@dlsu.edu.ph <cedric_daniel_chan@dlsu.edu.ph>
2026-07-11 15:10:02 +0800

    refactor: change webpack.config file to three different files referenced as common, dev, and prod for different development modes

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,16 @@
-// webpack.config.js
+// webpack.common.js
 import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 
 export default {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
   },
+  /*Insert plugins */
+
+  /*Insert modules*/
 };
